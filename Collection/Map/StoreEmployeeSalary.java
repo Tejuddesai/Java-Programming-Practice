0cc8acfa727b53cab6e32c923f565a65a126bc96
Tejuddesai <164405621+Tejuddesai@users.noreply.github.com>
2026-08-27 16:10:29 +0530

    sort employee

---
+++ Collection/Map/StoreEmployeeSalary.java
@@ -0,0 +1,15 @@
+
+
+//Create a Map containing employee names and salaries. 
+//Display all employees whose salary is greater than 50,000.
+
+package Map;
+import java.util.*;
+public class StoreEmployeeSalary {
+
+	public static void main(String[] args)
+	{
+		Map<String,Integer> map = new HashMap<>();
+		map.put("yyy",22);
+	}
+}
